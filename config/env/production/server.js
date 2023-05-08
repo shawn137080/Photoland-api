03612d35c028f0b3a5293acdece23ef0e71b027b
Shawn Pan <shawn137080@gmail.com>
2023-05-07 22:22:33 -0400

    add dep

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,7 @@
 module.exports = ({ env }) => ({
-    url: env('MY_GCR_URL'),
+    proxy: true,
+    url: env('APP_URL'), // Sets the public URL of the application.
+    app: { 
+      keys: env.array('APP_KEYS')
+    },
 });
